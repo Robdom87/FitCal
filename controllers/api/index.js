a5e2f13b6c2c10cafb939e0dfd2185354a9af0e1
Cedar Chavez <geggas00@gmail.com>
2022-10-12 19:14:26 -0700

    add fetch request serverside, and some class renaming

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+
 const sessionRoutes = require('./session-routes');
 const sessionWorkoutRoutes = require('./sessionWorkouts-routes');
 const nutritionRoutes = require('./nutritionRoutes');
+const exerciseRoutes = require('./exerciseRoutes');
 
 router.use('/session', sessionRoutes);
 router.use('/session/wkts', sessionWorkoutRoutes);
-router.use('/nutrition',nutritionRoutes)
+router.use('/nutrition',nutritionRoutes);
+router.use('/exercise',exerciseRoutes);
 
 module.exports = router;
