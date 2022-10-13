2922f89f443257e24abe16bb5c30fed6e80a46c2
Cedar Chavez <geggas00@gmail.com>
2022-10-13 15:18:49 -0700

    add program and program workouts routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,13 +5,16 @@ const sessionWorkoutRoutes = require('./sessionWorkouts-routes');
 const nutritionRoutes = require('./nutritionRoutes');
 const userRoutes = require("./userRoutes");
 const exerciseRoutes = require('./exerciseRoutes');
-
+const programRoutes = require('./program-routes');
+const programWorkoutRoutes = require('./programWorkouts-routes');
 
 router.use('/session', sessionRoutes);
 router.use('/session/wkts', sessionWorkoutRoutes);
 router.use('/nutrition',nutritionRoutes);
 router.use("/users", userRoutes);
 router.use('/exercise',exerciseRoutes);
+router.use('/program', programRoutes);
+router.use('/program/wkts', programWorkoutRoutes);
 
 
 module.exports = router;
