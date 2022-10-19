e40ef1a7ac283ed32390deb1d7485dc3e98c28b4
Cedar Chavez <geggas00@gmail.com>
2022-10-19 14:38:26 -0700

    add styling and sample data to nutrition page

--- public/assets/js/dietaryLog.js
+++ public/assets/js/dietaryLog.js
@@ -204,10 +204,10 @@ function saveMacros() {
 function printMacros() {
     let macrosSaved = JSON.parse(localStorage.getItem('macroRow'));
     console.log(macrosSaved);
-    $("#calMacros").text(macrosSaved.protein + " kcal");
-    $("#proteinMacros").text(macrosSaved.carbs + " g");
-    $("#carbMacros").text(macrosSaved.fat + " g");
-    $("#fatMacros").text(macrosSaved.calories + " g");
+    $("#calMacros").text(macrosSaved.calories + " kcal");
+    $("#proteinMacros").text(macrosSaved.protein + " g");
+    $("#carbMacros").text(macrosSaved.carbs + " g");
+    $("#fatMacros").text(macrosSaved.fat + " g");
 }
 
 //update local storage after removing row
@@ -232,6 +232,7 @@ function errorMessage() {
 //display information if any exists in local storage
 function init() {
     if (localStorage.getItem('nutritionRow') !== null) {
+        $('.sampleNutri').empty();
         printSavedNutrition();
     }
     let savedMacro = JSON.parse(localStorage.getItem('macroRow'));
